b0a9dee7cb13f2885215d986d7a2eca2abf7f56e
Sco2000 <ousmanemarra70@gmail.com>
2026-04-16 21:13:14 +0000

    fix deployment's errors

--- src/infrastructure/config/env.ts
+++ src/infrastructure/config/env.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+dotenv.config();
 
-export const PORT = process.env.PORT || 4000;
+export const PORT = process.env.PORT || 10000;
 export const SECRET_KEY = process.env.SECRET_KEY as string
--- src/infrastructure/http/server.ts
+++ src/infrastructure/http/server.ts
@@ -3,6 +3,6 @@ import { PORT } from "../config/env.js";
 import { startAllWorkers } from "../jobs/index.js";
 
 app.listen(Number(PORT), '0.0.0.0', () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on ${PORT}`);
   startAllWorkers()
 });
